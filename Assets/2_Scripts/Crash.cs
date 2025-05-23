82420b275549fb894f24f6f7600f7fea6ee0980b
최솔 <vahyeon2510@sonline20.sen.go.kr>
2025-05-23 15:20:05 +0900

    particle effect240번까지 완료

--- Assets/2_Scripts/Crash.cs
+++ Assets/2_Scripts/Crash.cs
@@ -3,12 +3,40 @@ using UnityEngine.SceneManagement;
 
 public class Crash : MonoBehaviour
 {
+    private GameObject crashEffectObj;
+    private ParticleSystem crashEffect;
+
+    private void Awake()
+    {
+        // 자식에서 "CrashEffect" 오브젝트와 ParticleSystem 찾기
+        crashEffectObj = transform.Find("CrashEffect")?.gameObject;
+        if (crashEffectObj != null)
+        {
+            crashEffect = crashEffectObj.GetComponent<ParticleSystem>();
+            crashEffectObj.SetActive(false); // 시작 시 비활성화
+        }
+    }
+
+    [SerializeField]
+    private float reloadDelay = 2f;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Ground"))
         {
+            if (crashEffectObj != null && crashEffect != null)
+            {
+                crashEffectObj.SetActive(true); // 오브젝트 활성화
+                crashEffect.Play();             // 이펙트 재생
+            }
+
             Debug.Log("오 내머리야");
-            SceneManager.LoadScene("0");
+            Invoke(nameof(ReloadScene), reloadDelay);
         }
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/2_Scripts/FinishLine.cs
+++ Assets/2_Scripts/FinishLine.cs
@@ -3,12 +3,37 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    private GameObject finalEffectObj;
+    private ParticleSystem finalEffect;
+
+    private void Awake()
+    {
+        // 자식에서 "FinalEffect" 오브젝트와 ParticleSystem 찾기
+        finalEffectObj = transform.Find("FinalEffect")?.gameObject;
+        if (finalEffectObj != null)
+        {
+            finalEffect = finalEffectObj.GetComponent<ParticleSystem>();
+            finalEffectObj.SetActive(false); // 시작 시 비활성화
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            if (finalEffectObj != null && finalEffect != null)
+            {
+                finalEffectObj.SetActive(true); // 오브젝트 활성화
+                finalEffect.Play();             // 이펙트 재생
+            }
+
             Debug.Log("완주 했습니다");
-            SceneManager.LoadScene("0");
+            Invoke(nameof(ReloadScene), 2f);
         }
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
